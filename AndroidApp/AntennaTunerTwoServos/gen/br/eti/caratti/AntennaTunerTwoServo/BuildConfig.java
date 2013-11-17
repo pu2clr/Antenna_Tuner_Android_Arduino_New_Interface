ff84312d49829f6b72baa8b509ee2e926c5faa36
Ricardo Lima Caratti <pu2clr@gmail.com>
2013-11-17 19:21:40 -0200

    First upload
    first upload

---
+++ AndroidApp/AntennaTunerTwoServos/gen/br/eti/caratti/AntennaTunerTwoServo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.eti.caratti.AntennaTunerTwoServo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
